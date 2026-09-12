eade163bdcd131c51838064f8e1c63bc7a8d8512
COLLEGELAB-07 <COLLEGELAB-07@COLLEGELAB-07>
2026-09-12 17:19:41 +0800

    Add project files.

---
+++ Reyes_CRUD/Models/Data/AppDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Reyes_CRUD.Models.Domain;
+
+namespace Reyes_CRUD.Models.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Product> Product => Set<Product>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+
+    }
+}
---
+++ Reyes_CRUD/Models/Dto/ProductDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Reyes_CRUD.Models.Dto
+{
+    public class ProductDTO
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int StockQuantity { get; set; }
+
+    }
+}
